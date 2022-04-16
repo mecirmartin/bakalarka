14410e32d7a9ac695fbe7daf43be442af71f9348
mecirmartin <mecir.martin@gmail.com>
2022-04-16 19:08:43 +0200

    fix: fix label

--- src/Application.ts
+++ src/Application.ts
@@ -1,121 +1,104 @@
-import * as SRD from "@projectstorm/react-diagrams"
+import * as SRD from "@projectstorm/react-diagrams";
 
-import { EntityNodeFactory } from "./nodes/entity/EntityFactory"
-import { RelationshipNodeFactory } from "./nodes/relationship/RelationshipFactory"
-import { AttributeNodeFactory } from "./nodes/attribute/AttributeFactory"
-import { AdvancedLinkFactory } from "./links/advancedlink/AdvancedLinkFactory"
-import { TriangleNodeFactory } from "./nodes/generalization-category/TriangleNodeFactory"
-import { States } from "./state/States"
-import { DefaultDiagramState } from "@projectstorm/react-diagrams"
-import { SimpleLinkFactory } from "./links/simplelink/SimpleLinkFactory"
-import { EditableLabelFactory } from "./links/editable-label/EditableLabelFactory"
-import Zoomaction from "./helpers/zoom"
-import { CustomDiagramModel } from "./customModel"
-import { CommandManager } from "./components/command-manager/commandManager"
+import { EntityNodeFactory } from "./nodes/entity/EntityFactory";
+import { RelationshipNodeFactory } from "./nodes/relationship/RelationshipFactory";
+import { AttributeNodeFactory } from "./nodes/attribute/AttributeFactory";
+import { AdvancedLinkFactory } from "./links/advancedlink/AdvancedLinkFactory";
+import { TriangleNodeFactory } from "./nodes/generalization-category/TriangleNodeFactory";
+import { States } from "./state/States";
+import { DefaultDiagramState } from "@projectstorm/react-diagrams";
+import { SimpleLinkFactory } from "./links/simplelink/SimpleLinkFactory";
+import { EditableLabelFactory } from "./links/editable-label/EditableLabelFactory";
+import Zoomaction from "./helpers/zoom";
+import { CustomDiagramModel } from "./customModel";
+import { CommandManager } from "./components/command-manager/commandManager";
 
 export class Application {
-  protected activeModel: SRD.DiagramModel
-  protected diagramEngine: SRD.DiagramEngine
+  protected activeModel: SRD.DiagramModel;
+  protected diagramEngine: SRD.DiagramEngine;
 
   constructor() {
-    this.activeModel = null
-    this.diagramEngine = SRD.default({ registerDefaultZoomCanvasAction: false })
-    this.newModel()
-    this.diagramEngine.getActionEventBus().registerAction(new Zoomaction())
-    this.diagramEngine.maxNumberPointsPerLink = 0
+    this.activeModel = null;
+    this.diagramEngine = SRD.default({ registerDefaultZoomCanvasAction: false });
+    this.newModel();
+    this.diagramEngine.getActionEventBus().registerAction(new Zoomaction());
+    this.diagramEngine.maxNumberPointsPerLink = 0;
 
     // Register factory
-    const factoriesManager = this.diagramEngine.getNodeFactories()
-    factoriesManager.registerFactory(new EntityNodeFactory())
-    factoriesManager.registerFactory(new RelationshipNodeFactory())
-    factoriesManager.registerFactory(new AttributeNodeFactory())
-    factoriesManager.registerFactory(new TriangleNodeFactory())
+    const factoriesManager = this.diagramEngine.getNodeFactories();
+    factoriesManager.registerFactory(new EntityNodeFactory());
+    factoriesManager.registerFactory(new RelationshipNodeFactory());
+    factoriesManager.registerFactory(new AttributeNodeFactory());
+    factoriesManager.registerFactory(new TriangleNodeFactory());
 
-    this.diagramEngine
-      .getLinkFactories()
-      .registerFactory(new AdvancedLinkFactory())
-    this.diagramEngine
-      .getLinkFactories()
-      .registerFactory(new SimpleLinkFactory())
-    this.diagramEngine
-      .getLabelFactories()
-      .registerFactory(new EditableLabelFactory())
-    this.diagramEngine.getStateMachine().pushState(new States())
-    const state = this.diagramEngine.getStateMachine().getCurrentState()
+    this.diagramEngine.getLinkFactories().registerFactory(new AdvancedLinkFactory());
+    this.diagramEngine.getLinkFactories().registerFactory(new SimpleLinkFactory());
+    this.diagramEngine.getLabelFactories().registerFactory(new EditableLabelFactory());
+    this.diagramEngine.getStateMachine().pushState(new States());
+    const state = this.diagramEngine.getStateMachine().getCurrentState();
 
-    ;(state as DefaultDiagramState).dragNewLink.config.allowLooseLinks = false
+    (state as DefaultDiagramState).dragNewLink.config.allowLooseLinks = false;
   }
 
   adjustGridOffset = ({ offsetX, offsetY }: any) => {
-    document.body.style.setProperty("--offset-x", `${Math.round(offsetX)}px`)
-    document.body.style.setProperty("--offset-y", `${Math.round(offsetY)}px`)
-  }
+    document.body.style.setProperty("--offset-x", `${Math.round(offsetX)}px`);
+    document.body.style.setProperty("--offset-y", `${Math.round(offsetY)}px`);
+  };
 
   adjustGridZoom = ({ zoom }: any) => {
-    const { gridSize } = this.activeModel.getOptions()
-    document.body.style.setProperty(
-      "--grid-size",
-      `${(gridSize * zoom) / 20}px`
-    )
-  }
+    const { gridSize } = this.activeModel.getOptions();
+    document.body.style.setProperty("--grid-size", `${(gridSize * zoom) / 20}px`);
+  };
 
   realignGrid = () => {
     this.adjustGridOffset({
       offsetX: this.activeModel.getOffsetX(),
       offsetY: this.activeModel.getOffsetY(),
-    })
+    });
 
     this.adjustGridZoom({
       zoom: this.activeModel.getZoomLevel(),
-    })
-  }
+    });
+  };
 
   public newModel() {
-    this.activeModel = new CustomDiagramModel()
-    this.activeModel.setLocked(false)
-    this.activeModel.setGridSize(1)
+    this.activeModel = new CustomDiagramModel();
+    this.activeModel.setLocked(false);
+    this.activeModel.setGridSize(1);
     this.activeModel.registerListener({
       eventDidFire: event => {
-        const type = event.function
-        if (type === "offsetUpdated") this.adjustGridOffset(event)
-        if (type === "zoomUpdated") this.adjustGridZoom(event)
+        const type = event.function;
+        if (type === "offsetUpdated") this.adjustGridOffset(event);
+        if (type === "zoomUpdated") this.adjustGridZoom(event);
       },
-    })
-    this.realignGrid()
-    this.diagramEngine.setModel(this.activeModel)
+    });
+    this.realignGrid();
+    this.diagramEngine.setModel(this.activeModel);
     // Add command manager
     //@ts-ignore
-    window.commandManager = new CommandManager()
+    window.commandManager = new CommandManager();
     // Add command manager event listeners
     window.addEventListener("keydown", (event: any) => {
-      console.log("ev", event)
-      if (
-        event.keyCode === 90 &&
-        (event.ctrlKey || event.metaKey) &&
-        !event.shiftKey
-      ) {
+      console.log("ev", event);
+      if (event.keyCode === 90 && (event.ctrlKey || event.metaKey) && !event.shiftKey) {
         // @ts-ignore
-        window.commandManager.undo()
-        this.diagramEngine.repaintCanvas()
+        window.commandManager.undo();
+        this.diagramEngine.repaintCanvas();
       }
 
-      if (
-        event.keyCode === 90 &&
-        (event.ctrlKey || event.metaKey) &&
-        event.shiftKey
-      ) {
+      if (event.keyCode === 90 && (event.ctrlKey || event.metaKey) && event.shiftKey) {
         // @ts-ignore
-        window.commandManager.redo()
-        this.diagramEngine.repaintCanvas()
+        window.commandManager.redo();
+        this.diagramEngine.repaintCanvas();
       }
-    })
+    });
   }
 
   public getActiveDiagram(): SRD.DiagramModel | null {
-    return this.activeModel
+    return this.activeModel;
   }
 
   public getDiagramEngine(): SRD.DiagramEngine {
-    return this.diagramEngine
+    return this.diagramEngine;
   }
 }
--- src/links/editable-label/EditableLabelModel.ts
+++ src/links/editable-label/EditableLabelModel.ts
@@ -1,52 +1,46 @@
-import { LabelModel } from "@projectstorm/react-diagrams"
-import {
-  BaseModelOptions,
-  DeserializeEvent,
-} from "@projectstorm/react-canvas-core"
+import { LabelModel } from "@projectstorm/react-diagrams";
+import { BaseModelOptions, DeserializeEvent } from "@projectstorm/react-canvas-core";
 
 export interface EditableLabelOptions extends BaseModelOptions {
-  extras?: string
+  extras?: string;
 }
 
 interface Extras {
-  value: string
-  type: "MULTIPLICITY" | "ROLE"
+  value: string;
+  type: "MULTIPLICITY" | "ROLE";
 }
 
 export class EditableLabelModel extends LabelModel {
-  private extras: Extras
+  private extras: Extras;
 
   constructor(labelState: Extras) {
     super({
       ...labelState,
       type: "editable-label",
-    })
+    });
     this.extras = {
-      value:
-        labelState.value || labelState.type === "MULTIPLICITY"
-          ? "1..n"
-          : "Role",
+      value: labelState.value || labelState.type === "MULTIPLICITY" ? "1" : "Role",
       type: labelState.type,
-    }
+    };
   }
 
   serialize() {
     return {
       ...super.serialize(),
       extras: this.extras,
-    }
+    };
   }
 
   setState(extras: Extras) {
-    this.extras = extras
+    this.extras = extras;
   }
 
   getState() {
-    return this.extras
+    return this.extras;
   }
 
   deserialize(event: DeserializeEvent<this>): void {
-    super.deserialize(event)
-    this.extras = event.data.extras
+    super.deserialize(event);
+    this.extras = event.data.extras;
   }
 }
