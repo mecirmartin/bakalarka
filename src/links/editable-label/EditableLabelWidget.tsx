1468d939a9e8e5e33404587bb3e4adb4bfce33d0
mecirmartin <mecir.martin@gmail.com>
2022-05-19 12:53:42 +0200

    feat: showLabel

--- src/links/editable-label/EditableLabelWidget.tsx
+++ src/links/editable-label/EditableLabelWidget.tsx
@@ -1,40 +1,53 @@
-import * as React from "react"
+import * as React from "react";
 
-import { EditableLabelModel } from "./EditableLabelModel"
-import styled from "@emotion/styled"
-import { InlineEdit } from "../../components/inline-edit/InlineEdit"
-import { useEffect, useState } from "react"
-import { useForceUpdate } from "../../components/BodyWidget"
+import { EditableLabelModel } from "./EditableLabelModel";
+import styled from "@emotion/styled";
+import { InlineEdit } from "../../components/inline-edit/InlineEdit";
+import { useEffect, useRef, useState } from "react";
+import { useForceUpdate } from "../../components/BodyWidget";
 
 export interface FlowAliasLabelWidgetProps {
-  model: EditableLabelModel
+  model: EditableLabelModel;
 }
 
 export const Label = styled.div<{ showLabel: boolean }>`
   user-select: none;
   pointer-events: auto;
   display: ${props => (props.showLabel ? "box" : "none")};
-`
+`;
 
 // now we can render all what we want in the label
-export const EditableLabelWidget: React.FunctionComponent<
-  FlowAliasLabelWidgetProps
-> = props => {
+export const EditableLabelWidget: React.FunctionComponent<FlowAliasLabelWidgetProps> = props => {
   const [state, setState] = useState({
     value: props.model.getState().value || "ROLE",
     type: props.model.getState().type,
-  })
-  const forceUpdate = useForceUpdate()
+  });
+  const [showLabel, setShowLabel] = useState(true);
+  const labelRef = useRef(null);
+  const forceUpdate = useForceUpdate();
 
   useEffect(() => {
-    props.model.setState(state)
-    forceUpdate()
-  }, [state])
+    props.model.setState(state);
+    forceUpdate();
+  }, [state]);
 
-  console.log("state", state)
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (
+        labelRef.current &&
+        !labelRef.current.contains(event.target) &&
+        !props.model.getState().value
+      ) {
+        setShowLabel(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [labelRef]);
 
   return (
-    <Label showLabel={!!props.model.getState().value}>
+    <Label ref={labelRef} showLabel={showLabel}>
       <InlineEdit
         style={{
           color: "#fff",
@@ -47,5 +60,5 @@ export const EditableLabelWidget: React.FunctionComponent<
         setState={setState}
       />
     </Label>
-  )
-}
+  );
+};
